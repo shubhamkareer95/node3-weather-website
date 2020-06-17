d4740827398e280d00bbab72a39d75ac22096cdd
Shubham Kareer <shubhamkareer@Shubhams-MacBook-Pro.local>
2020-06-17 20:06:32 +0530

    Add data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -33,11 +33,14 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        "It is currently " +
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
           body.current.temperature +
           " degrees out. There wind speed feels like " +
           body.current.feelslike +
-          " degrees out"
+          " degrees out. The humidity is " +
+          body.current.humidity +
+          "%."
       );
     }
   });
